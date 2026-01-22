9651c671a74db188bf2cb3e600d0d37d9e8b7cf9
Trent Chipman <trentchip@gmail.com>
2026-01-22 14:28:04 -0700

    Adding LEDPinOutput example and updating README

---
+++ LEDPinOutput/LEDPinOutput.ino
@@ -0,0 +1,28 @@
+/*
+  Blink an external LED
+  You should connect your LED in the correct direction to the specified pin with a 1k+ ohm resistor.
+*/
+
+//Global variables to use
+const int mDelay = 500;//You can change this value
+//mDelay is a variable that will set how long the LED blinks for
+const int LEDPin = 6;
+//From now on "LEDPin" will be an alias for pin 10 on the Arduino Nano
+
+//Setup function runs once when device is powered on
+void setup() {
+  //Initialize output pin
+  pinMode(LEDPin, OUTPUT);
+}
+
+//Loop function runs over and over after setup
+void loop() {
+  //Turn the LED on (HIGH voltage)
+  digitalWrite(LEDPin, HIGH);
+  //Wait for set delay time
+  delay(mDelay);
+  //Turn the LED off with voltage LOW
+  digitalWrite(LEDPin, LOW);
+  //Wait for set delay time
+  delay(mDelay);
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ArduinoActivities
 This repo will hold code and circuit diagrams for a few simple Arduino projects. It will be used as the accompanying link for more information on the SPAC(IEEE) 2026 Microcontrollers workshop.
+
+# List of Projects
+1. TestYourNano/BlinkFast
+2. LEDPinOutput
+3. ButtonInput
+4. BuzzerTone
+5. MusicBox
+6. MemoryGame
