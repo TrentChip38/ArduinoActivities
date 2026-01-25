904e3db6d70df8ab139fdd923663d5a6fe121c6b
Trent Chipman <trentchip@gmail.com>
2026-01-25 16:22:22 -0700

    Change pin and variable name

--- 2_LEDPinOutput/2_LEDPinOutput.ino
+++ 2_LEDPinOutput/2_LEDPinOutput.ino
@@ -6,23 +6,23 @@
 //Global variables to use
 const int mDelay = 500;//You can change this value
 //mDelay is a variable that will set how long the LED blinks for
-const int LEDPin = 6;
+const int ledPin = 2;
 //From now on "LEDPin" will be an alias for pin 10 on the Arduino Nano
 
 //Setup function runs once when device is powered on
 void setup() {
   //Initialize output pin
-  pinMode(LEDPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
 }
 
 //Loop function runs over and over after setup
 void loop() {
   //Turn the LED on (HIGH voltage)
-  digitalWrite(LEDPin, HIGH);
+  digitalWrite(ledPin, HIGH);
   //Wait for set delay time
   delay(mDelay);
   //Turn the LED off with voltage LOW
-  digitalWrite(LEDPin, LOW);
+  digitalWrite(ledPin, LOW);
   //Wait for set delay time
   delay(mDelay);
 }
